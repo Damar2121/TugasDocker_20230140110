af6d037b49d8fbddd262da70a107d7441da494a6
Damar2121 <damar.vivoy51@gmail.com>
2026-04-14 09:57:56 +0700

    menambah file User.java beserta isinya

--- src/test/java/com/tugas/deploy/model/User.java
+++ src/test/java/com/tugas/deploy/model/User.java
@@ -1,4 +1,32 @@
 package com.tugas.deploy.model;
 
 public class User {
+    private String nama;
+    private String nim;
+    private String jenisKelamin;
+
+    // Getter dan Setter
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
 }
